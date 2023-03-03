5ad84d606f4a107ca1574ccb4c3dec1ae913eaae
Robert Vogel <bobbyvogel@icloud.com>
2023-03-02 16:10:19 -0800

    eliminated accidental file named ':' and updated eliminated automatic module load in top level __ini__.

--- pySUMMA/__init__.py
+++ pySUMMA/__init__.py
@@ -1,8 +1,8 @@
-from os import path
+# from os import path
 
 __version__ = "0.3.1"
 
-from .sml import Sml
-from .summa import Summa
-from .woc import BinaryWoc
-from .woc import RankWoc
+# from .sml import Sml
+# from .summa import Summa
+# from .woc import BinaryWoc
+# from .woc import RankWoc
